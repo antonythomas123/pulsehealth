a70ac5f4bbeddfc0c22800f9f64e3e95eeaec531
antonythomas123 <antonythomas993@gmail.com>
2026-03-28 12:36:56 +0530

    Button component

--- main/src/components/index.ts
+++ main/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Card } from "./Card";
+export { default as Button } from "./Button";
 export { default as TextField } from "./TextField";
 export { default as Select } from "./Select";
 export { default as ErrorBoundary } from "./ErrorBoundary";
